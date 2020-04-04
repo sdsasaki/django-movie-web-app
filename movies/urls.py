ac9c769e9651e65475ab906c9506bc3c9d24b029
sdsasaki <seandsasaki@gmail.com>
2020-04-04 00:06:38 +0000

    Created django movie web app

---
+++ movies/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home_page, name='home_page'),
+    path('create/', views.create, name='create'),
+    path('edit/<str:movie_id>', views.edit, name='edit'),
+    path('delete/<str:movie_id>', views.delete, name='delete')
+]
+
---
+++ movies/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from airtable import Airtable
+import os
+
+AT = Airtable(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID'),
+             'Movies',
+             api_key=os.environ.get('AIRTABLE_API_KEY'))
+
+# Create your views here.
+def home_page(request):
+    user_query = str(request.GET.get('query', ''))
+    search_result = AT.get_all(formula="FIND('" + user_query.lower() + "', LOWER({Name}))")
+    stuff_for_frontend = {'search_result': search_result}
+    return render(request, 'movies/movies_stuff.html', stuff_for_frontend)
+
+def create(request):
+    if request.method == 'POST':
+        data = {
+            'Name': request.POST.get('name'),
+            'Pictures': [{'url': request.POST.get('url') or 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/No_picture_available.png/401px-No_picture_available.png'}],
+            'Rating': int(request.POST.get('rating')),
+            'Notes': request.POST.get('notes')
+        }
+
+        try:
+            response = AT.insert(data)
+            messages.success(request, 'New movie added: {}'.format(response['fields'].get('Name')))
+        except Exception as e:
+            messages.warning(request, 'Got an eror when trying to create a new movie: {}'.format(e))
+
+    return redirect('/')
+
+def edit(request, movie_id):
+    if request.method == 'POST':
+        data = {
+            'Name': request.POST.get('name'),
+            'Pictures': [{'url': request.POST.get('url') or 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/No_picture_available.png/401px-No_picture_available.png'}],
+            'Rating': int(request.POST.get('rating')),
+            'Notes': request.POST.get('notes')
+        }
+        try:
+            response = AT.update(movie_id, data)
+            messages.success(request, 'Updated movie: {}'.format(response['fields'].get('Name')))
+        except Exception as e:
+            messages.warning(request, 'Got an error when trying to update a movie: {}'.format(e))
+
+    return redirect('/')
+
+def delete(request, movie_id):
+    try:
+        movie_name = AT.get(movie_id)['fields'].get('Name')
+        response = AT.delete(movie_id)
+        messages.warning(request, 'Deleted movie: {}'.format(movie_name))
+    except Exception as e:
+        messages.warning(request, 'Got an error when trying to delete a movie: {}'.format(e))
+
+    return redirect('/')
